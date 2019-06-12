5ac822605eb3bce7109fc841177a5836b88d0687
sejinkay <sejinkay@gmail.com>
2019-06-12 16:53:24 +0100

    added Createmole function to the MouseClick script
    Co-Authored-By: Gui Heurich <guiheurich@gmail.com>
    Co-Authored-By: Marcus Clairmont <marcusclairmont@gmail.com>
    Co-Authored-By: Mat Shup <matmoon14@gmail.com>

--- Guacamole/Assets/Scripts/MouseClick.cs
+++ Guacamole/Assets/Scripts/MouseClick.cs
@@ -5,21 +5,29 @@ using UnityEngine.UI;
 
 public class MouseClick : MonoBehaviour
 {
+    public GameObject mole;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mole.gameObject.SetActive(true);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnMouseDown()
     {
         ScoreUpdate.scoreValue += 100;
-        Destroy(gameObject);
+        mole.gameObject.SetActive(false);
+        Invoke("CreateMole", 2);
+    }
+
+    void CreateMole()
+    {
+        mole.gameObject.SetActive(true);
     }
 }
