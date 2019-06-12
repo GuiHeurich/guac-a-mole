90c99c4674e8e0e9ce88254a22389ec2d0a53bcc
sejinkay <sejinkay@gmail.com>
2019-06-12 11:24:11 +0100

    Implement User Stories 1 to 5
    As a game player,
    So that I can play whack-a-mole
    I would like to be able to see a mole on the screen
    
    As a game player,
    So that I can whack a mole
    I would like to be able to click or touch the mole
    
    As a game player,
    So that I know I've whacked a mole
    I would like the mole to disappear when I click it
    
    As a game player,
    So that I can have an idea of how well I'm doing
    I would like to see my points
    
    As a game player,
    So that I can have an idea of how well I'm doing
    I would like to see my points updated
    
    Co-Authored-By: Gui Heurich <guiheurich@gmail.com>
    Co-Authored-By: Marcus Clairmont <marcusclairmont@gmail.com>
    Co-Authored-By: Mat Shup <matmoon14@gmail.com>

---
+++ Guacamole/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        ScoreUpdate.scoreValue += 100;
+        Destroy(gameObject);
+    }
+}
