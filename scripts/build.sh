8cd521559e6d61bfab964fb0372bf6a4baf04d06
guilhe0756 <guiheurich@gmail.com>
2019-06-17 11:59:50 +0100

    Change Install.sh configurations

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
 project="Guac-a-mole"
-
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
+#
+# echo "Attempting to build $project for Windows"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -nographics \
+#   -silent-crashes \
+#   -logFile $(pwd)/unity.log \
+#   -projectPath $(pwd) \
+#   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+#   -quit
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -22,15 +22,15 @@ echo "Attempting to build $project for OS X"
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
+# echo "Attempting to build $project for Linux"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -nographics \
+#   -silent-crashes \
+#   -logFile $(pwd)/unity.log \
+#   -projectPath $(pwd) \
+#   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+#   -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,39 @@
 #! /bin/sh
 
-# This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
-# installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-echo 'Downloading from https://unity3d.com/get-unity/download?thank-you=update&download_nid=62611&os=Mac: '
-curl -o Unity.pkg https://unity3d.com/get-unity/download?thank-you=update&download_nid=62611&os=Mac
+# See https://unity3d.com/get-unity/download/archive
+# to get download URLs
+UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
+UNITY_OSX_PACKAGE_URL='https://download.unity3d.com/download_unity/f2970305fe1c/UnityDownloadAssistant.dmg'
 
-echo 'Installing Unity'
-sudo installer -dumplog -package Unity.pkg -target /
+# Downloads a file if it does not exist
+download() {
+
+	URL=$1
+	FILE=`basename "$URL"`
+
+	# Downloads a package if it does not already exist in cache
+	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$URL"` ] ; then
+		echo "$FILE does not exist. Downloading from $URL: "
+		mkdir -p "$UNITY_DOWNLOAD_CACHE"
+		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$URL"` "$URL"
+	else
+		echo "$FILE Exists. Skipping download."
+	fi
+}
+
+# Downloads and installs a package from an internet URL
+install() {
+	PACKAGE_URL=$1
+	download $1
+
+	echo "Installing `basename "$PACKAGE_URL"`"
+	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE_URL"` -target /
+}
+
+
+
+echo "Contents of Unity Download Cache:"
+ls $UNITY_DOWNLOAD_CACHE
+
+echo "Installing Unity..."
+install $UNITY_OSX_PACKAGE_URL
